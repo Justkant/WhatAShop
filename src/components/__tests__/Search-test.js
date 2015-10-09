78fab06ef74130a2d0bb71373ac6a59a26b6d371
Justkant <quent92100@gmail.com>
2015-10-09 15:57:24 +0800

    Fix npm test failed

--- src/components/__tests__/Search-test.js
+++ src/components/__tests__/Search-test.js
@@ -3,6 +3,8 @@ import {renderIntoDocument} from 'react-addons-test-utils';
 import { expect} from 'chai';
 import { Search } from 'components';
 import { Provider } from 'react-redux';
+import {reduxReactRouter} from 'redux-router';
+import createHistory from 'history/lib/createMemoryHistory';
 import createStore from 'redux/create';
 import ApiClient from 'helpers/ApiClient';
 const client = new ApiClient();
@@ -19,7 +21,7 @@ describe('Search', () => {
     }
   };
 
-  const store = createStore(client, mockStore);
+  const store = createStore(reduxReactRouter, null, createHistory, client, mockStore);
   const renderer = renderIntoDocument(
     <Provider store={store} key="provider">
       <Search/>
